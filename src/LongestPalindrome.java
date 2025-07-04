b0489cb00eaeb6f99f6358a405754d66000f92dd
sarthak4504 <sarthak.pathak4504@gmail.com>
2025-07-04 23:20:06 +0530

    LinkedListCycle Solution

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class LongestPalindrome {
+public class LongestPalindrome {
+        public int longestPalindrome(String s) {
+            int [] freq=new int[128];
+            char [] charArr=s.toCharArray();
+
+            for(int i=0;i<s.length();i++) freq[charArr[i]]++;
+
+            int res=0;
+            boolean odd=false;
+
+            for(int i=0;i<128;i++){
+                if(freq[i]%2==0) res+=freq[i];
+                else {
+                    res+=freq[i]-1;
+                    odd=true;
+                }
+            }
+
+            if(odd) res+=1;
+            return res;
+
+        }
 }
--- src/PalindromeLL.java
+++ src/PalindromeLL.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class PalindromeLL {
+public class PalindromeLL {
+    public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode left=head;
+        ListNode right= reverseList(slow);
+        while(right!=null){
+            if(left.val!=right.val) return false;
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+    public static ListNode reverseList(ListNode head) {
+        ListNode temp=head;
+        ListNode prev=null;
+        while(temp!=null){
+            ListNode curr=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=curr;
+        }
+        return prev;
+    }
 }
